3778f8cf461aff76b98940f63f8b0b7f77faf263
Nando J. <fernando.jayae2004@gmail.com>
2025-04-15 20:43:27 -0500

    ola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HijoComponent } from "./hijo/hijo.component";
 import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HijoComponent, PadreComponent],
+  imports: [PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
